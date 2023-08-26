c3d5e522597ad7568c4523eb45e74a1104f955b6
unknown <iverfinne@gmail.com>
2023-08-26 21:27:22 +0200

    a

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ module.exports = {
   experimental: {
     serverActions: true,
   },
+  images: {
+    domains: ['geotag.club', '*.geotag.club', 'avatars.githubusercontent.com', 'public.blob.vercel-storage.com', 'lossless.geotag.club', 'akima.geotag.club', ],
+  },
   reactStrictMode: false,
 };
