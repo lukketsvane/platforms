8f734653de68e86562ded906266eb00be19fe60b
unknown <iverfinne@gmail.com>
2023-08-26 20:52:23 +0200

    add domains to images

--- next.config.js
+++ next.config.js
@@ -5,8 +5,5 @@ module.exports = {
   experimental: {
     serverActions: true,
   },
-  images: {
-    domains: ['geotag.club', '*.geotag.club', 'avatars.githubusercontent.com', 'public.blob.vercel-storage.com', 'lossless.geotag.club', 'akima.geotag.club'],
-  },
   reactStrictMode: false,
 };
